16ab50d1194e289afece35b3f7bf95818c1fc46a
Josh Utley <josh@intrepidnetwork.us>
2013-09-17 18:04:03 -0700

    These function are not needed for this site

--- htdocs/sites/all/themes/intrepidnetwork/template.php
+++ htdocs/sites/all/themes/intrepidnetwork/template.php
@@ -7,11 +7,11 @@
   }
 }*/
 
-function intrepidnetwork_form_alter(&$form, &$form_state, $form_id) {
+/*function intrepidnetwork_form_alter(&$form, &$form_state, $form_id) {
   if ($form_id == 'webform_client_form_65') {
     $form['#attributes']['class'][] = 'row-fluid';
   }
 	if (!empty($form['actions']) && $form['actions']['submit']) {
     $form['actions']['submit']['#attributes'] = array('class' => array('btn-large btn-inverse'));
   }
-}
+}*/
